d1f04a39fcbeba8fbc4a657010cf418d5bf66bbf
Nicholas DeVeau <nickdeveau3@outlook.com>
2024-09-04 17:03:48 -0400

    Initial Commit

---
+++ Stickies+/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Stickies+
+//
+//  Created by Nick DeVeau on 9/4/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
